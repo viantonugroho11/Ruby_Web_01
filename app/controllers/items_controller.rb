a4d2d423bb52e9a4defcd9a11f258a84a39b31ce
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-03-28 16:34:50 +0700

    fixing delet

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,4 +22,18 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(params.require(:item).permit(:name, :price))
+
+    redirect_to items_path
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+
+    redirect_to items_path
+  end
 end
